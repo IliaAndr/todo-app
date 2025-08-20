f731d4a61110c7170693b398441e7dad755fbb01
iliaandrianov <ilia.i.andrianov@gmail.com>
2025-08-20 11:15:50 +0300

    Remove prod.env from repo

---
+++ drafts/draft.ts
@@ -0,0 +1,10 @@
+/*const footerTaskLeftLocator = page.getByTestId('footer').locator('span');
+let textFromElement = await footerTaskLeftLocator.textContent();
+
+if (textFromElement !== null) {
+    const numberFromText = textFromElement.match(/\d+/);
+    const number = Number(numberFromText)
+    console.log(`Extracted number: ${number}`);
+} else {
+    console.log('Text content is null')
+}*/
